697a9739e3f7ed21e437b9bb6b6b0ab8ea0d853e
Yu Hou <hy1995@gmail.com>
2020-03-10 22:55:19 +1100

    use stdio transport as erlang_ls currently supports it
    deprecate all the old TCP socket code, and use stdio transport now

--- src/index.ts
+++ src/index.ts
@@ -4,81 +4,32 @@ import {
   workspace,
   ExtensionContext,
   LanguageClientOptions,
-  StreamInfo,
-  ProvideCompletionItemsSignature
+  TransportKind,
+  ServerOptions
 } from 'coc.nvim'
-import {
-  CompletionContext,
-  TextDocument,
-  Position,
-  CompletionItem,
-  CompletionList
-} from 'vscode-languageserver-protocol'
-import {CancellationToken} from 'vscode-jsonrpc'
-import {waitForSocket} from 'socket-retry-connect';
-import {execFile} from 'child_process'
 
 let client: LanguageClient
-
-const logger = workspace.createOutputChannel("coc-erlang")
-
-function startServer(serverPath: string, serverPort: number) {
-  return function () {
-    execFile(serverPath, [serverPort.toString()]);
-
-    logger.appendLine('waiting for erlang_ls to accept connection..');
-
-    return new Promise<StreamInfo>((resolve, reject) => {
-      waitForSocket({port: serverPort}, function (err, socket) {
-        if (err) {
-          reject(err);
-        }
-        else {
-          logger.appendLine('socket accepted, continuing.');
-          resolve({reader: socket, writer: socket});
-        }
-      });
-    });
-  };
-};
 
 export async function activate(context: ExtensionContext): Promise<void> {
   const config: WorkspaceConfiguration = workspace.getConfiguration("erlang_ls")
   const server_path: string = config.get<string>("erlang_ls_path")
-  const server_port: number = Number(config.get<number>("port"))
 
   let clientOptions: LanguageClientOptions = {
     documentSelector: [{scheme: 'file', language: 'erlang'}],
-    middleware: {
-      provideCompletionItem: async (
-        document: TextDocument,
-        position: Position,
-        context: CompletionContext,
-        token: CancellationToken,
-        next: ProvideCompletionItemsSignature
-      ) => {
-        const res = await Promise.resolve(next(document, position, context, token));
-        let doc = workspace.getDocument(document.uri);
-        if (!doc)
-          return [];
-
-        if (res.hasOwnProperty('isIncomplete')) {
-          let itemList = res as CompletionList;
-          itemList.items.forEach(item => fixItem(item, position))
-          return itemList;
-        }
-
-        let items = res as CompletionItem[];
-        items.forEach(item => fixItem(item, position))
-        return items;
-      }
-    },
     initializationOptions: ""
+  };
+
+  let serverArgs = ["--transport", "stdio"];
+
+  let serverOptions: ServerOptions = {
+    command: server_path,
+    args: serverArgs,
+    transport: TransportKind.stdio
   };
 
   client = new LanguageClient(
     "erlang_ls",
-    startServer(server_path, server_port),
+    serverOptions,
     clientOptions
   )
   client.start()
@@ -87,13 +38,6 @@ export async function activate(context: ExtensionContext): Promise<void> {
     client.registerProposedFeatures()
     workspace.showMessage("coc-erlang_ls is ready")
   })
-}
-
-function fixItem(i: CompletionItem, pos: Position) {
-  i.textEdit = {
-    range: {start: pos, end: pos},
-    newText: i.label.replace(/\/[^}]*/, '')
-  }
 }
 
 export function deactivate(): Thenable<void> | undefined {
